ab7062b48bd382293306ebabc82bbf272102c9c6
Chennade Brown <cjbrow25@ncsu.edu>
2021-10-23 22:19:25 -0400

    Chennade's fourth commit + alex first draft & deleted html file

--- Render Code.R
+++ Render Code.R
@@ -1,3 +1,6 @@
 rmarkdown:: render("Project 2 Rmd.Rmd",
                    output_format = "github_document",
-                   output_file = "Project2.md")
+                   output_file = "Project2.md",
+                   output_options = list(html_preview = FALSE, keep_html = FALSE, toc = TRUE, toc_depth = "1"))
+
+
