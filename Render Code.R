fe0e02f3541e02356f972f7168efee32901adf87
Alex Prevatte <awprevatte@.gmail.com>
2021-10-27 13:10:39 -0400

    .html to .md

--- Render Code.R
+++ Render Code.R
@@ -7,7 +7,7 @@ Topic <- list("Lifestyle", "Business", "Entertainment", "Social Media",
               "Tech", "World")
 
 # Create list for output files
-output_file <- paste0(Topic, ".md")
+output_file <- paste0("Reports/", Topic, ".md")
 params <- lapply(Topic, FUN = function(x){list(topic = x)})
 reports <- tibble(output_file, params)
 
@@ -15,7 +15,7 @@ reports <- tibble(output_file, params)
 library(rmarkdown)
 apply(reports, MARGIN = 1, 
       FUN = function(x){
-        render("Project2.Rmd", output_file = x[[1]], params = x[[2]])
+        render("Project 2 Rmd.Rmd", output_file = x[[1]], params = x[[2]])
       })
 
 
