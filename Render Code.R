053a625b8e9255fce0f1be94cfb5a1184dc3f08d
Alex Prevatte <awprevatte@.gmail.com>
2021-10-27 10:25:54 -0400

    Automation in render code

--- Render Code.R
+++ Render Code.R
@@ -1,6 +1,22 @@
-rmarkdown:: render("Project 2 Rmd.Rmd",
-                   output_format = "github_document",
-                   output_file = "Project2.md",
-                   output_options = list(html_preview = FALSE, keep_html = FALSE, toc = TRUE, toc_depth = "2"))
+# Authors: Alex Prevatte and Chennade Brown
+# Date: 10/31/21
+
+# Create variable that contains each article topic 
+library(tidyverse)
+Topic <- list("Lifestyle", "Business", "Entertainment", "Social Media",
+              "Tech", "World")
+
+# Create list for output files
+output_file <- paste0(Topic, ".html")
+params <- lapply(Topic, FUN = function(x){list(topic = x)})
+reports <- tibble(output_file, params)
+
+# Use apply function to render through each article topic
+library(rmarkdown)
+apply(reports, MARGIN = 1, 
+      FUN = function(x){
+        render("Project2.Rmd", output_file = x[[1]], params = x[[2]])
+      })
+
 
 
